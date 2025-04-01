7283c6ed2e7d59fb890e313766b41614ad51c2be
chris carter <chris.carter@icivics.org>
2025-04-01 11:53:45 -0600

    add nvim surround for html tags

--- init.lua
+++ init.lua
@@ -256,6 +256,16 @@ require('lazy').setup({
     },
   },
   {
+    'kylechui/nvim-surround',
+    version = '^3.0.0', -- Use for stability; omit to use `main` branch for the latest features
+    event = 'VeryLazy',
+    config = function()
+      require('nvim-surround').setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
+  {
     'kdheepak/lazygit.nvim',
     lazy = true,
     cmd = {
