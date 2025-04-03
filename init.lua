c10449a0fdf824b1bb741278d9ad27736ecf8c10
chris.carter <chris.carter@icivics.org>
2025-04-03 17:49:39 -0600

    turn on relativenumber because prime says it good

--- init.lua
+++ init.lua
@@ -103,7 +103,7 @@ vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
 
-vim.opt.relativenumber = false
+vim.opt.relativenumber = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.wrap = false
